d6c963a1e6b4c3f0a2e7907193ea4ff7742f5730
nancymcw <nancymcwhite@gmail.com>
2024-12-23 17:58:04 -0500

    Added React Bootstrap list to ServicesList component

--- src/Components/ServicesList.jsx
+++ src/Components/ServicesList.jsx
@@ -1,3 +1,13 @@
+import { ListGroup } from "react-bootstrap";
+
 export default function ServicesList() {
-  return <div>Services table</div>;
+  return (
+    <ListGroup className="services-list">
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+  );
 }
