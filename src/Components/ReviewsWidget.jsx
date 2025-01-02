f3df8ec3fba2a25a357364e630deb161841d1472
nancymcw <nancymcwhite@gmail.com>
2025-01-01 22:33:21 -0500

    Attempting to make widget work inside component, left URL at top of page to continue later

--- src/Components/ReviewsWidget.jsx
+++ src/Components/ReviewsWidget.jsx
@@ -1,12 +1,35 @@
-export function ReviewsWidget() {
-  // ----- REPUSO -----
-  const script = document.createElement("script");
-  script.type = "module";
-  script.src = "https://widgets.revue.us/2.0/rw-widget-list.js";
-  document.getElementsByTagName("head")[0].appendChild(script);
+// https://stackoverflow.com/questions/34424845/adding-script-tag-to-react-jsx
 
-  return <div data-rw-list="44697"></div>;
+import { useEffect } from "react";
+
+export function ReviewsWidget() {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    // script.src = "https://widgets.revue.us/2.0/rw-widget-list.js";
+    script.src =
+      "https://app.reviewgrower.com/js/v1/embed.js?token=55fd2b3b-0024-4069-beaa-92346db03a13";
+    script.type = "text/javascript";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  //   <div data-rw-list="44697"></div>;
 }
-{
-  /* <script src="https://app.reviewgrower.com/js/v1/embed.js?token=55fd2b3b-0024-4069-beaa-92346db03a13" type="text/javascript"></script> */
-}
+
+//   {
+//   const script = document.createElement("script");
+//   script.type = "module";
+//   script.src = "https://widgets.revue.us/2.0/rw-widget-list.js";
+//   document.getElementsByTagName("head")[0].appendChild(script);
+
+//   return <div data-rw-list="44697"></div>;
+// }
+// {
+//   <script src="https://app.reviewgrower.com/js/v1/embed.js?token=55fd2b3b-0024-4069-beaa-92346db03a13" type="text/javascript"></script>
+// }
