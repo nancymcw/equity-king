584c06f5c315a5f63feca6df4f358379590135d4
nancymcw <nancymcwhite@gmail.com>
2025-04-01 12:56:50 -0400

    Added gallery image with proper classname for div and img

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,49 +1,7 @@
 export function Gallery() {
   return (
     <div className="gallery-div">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum aperiam,
-      consequuntur optio nam quibusdam corporis atque quae. Animi facilis, modi
-      alias ducimus cumque voluptatem maiores mollitia beatae, recusandae
-      molestias, tempora provident. Voluptates, aliquid, beatae quas quibusdam
-      autem debitis quis rem officia, repudiandae consectetur tenetur nemo
-      nobis. Animi odit dolores esse dolore recusandae vero, asperiores quae
-      omnis illo ipsum quia inventore itaque consequuntur odio repellendus
-      tempore corrupti velit explicabo commodi ipsa tenetur? Maxime similique
-      dolores adipisci iure aliquid nobis, sequi unde eius. Architecto eos modi
-      consequuntur, ratione in reiciendis repellendus harum eaque dolor maiores
-      pariatur esse deserunt veritatis facere quod ducimus voluptas aliquid
-      perferendis praesentium? Neque sunt iusto saepe error rerum repellendus
-      reiciendis modi quod, dicta tenetur illo soluta porro eum iure in quas
-      eaque corrupti harum nam, aspernatur, atque corporis numquam nulla.
-      Maiores porro tempora voluptates facilis veniam dicta excepturi modi
-      commodi ducimus aspernatur amet ab suscipit architecto corporis, officia
-      fuga iste corrupti blanditiis fugit. Nisi excepturi, rem aspernatur
-      accusamus qui consequuntur quasi dolores quae veniam ab placeat quos
-      sapiente consectetur, impedit sed voluptatibus eveniet doloribus veritatis
-      molestiae! Nisi architecto, dolores, ducimus atque magnam dolore, dolor
-      pariatur itaque molestias ad odio sint veritatis voluptatibus sunt
-      praesentium earum enim iusto? Repellat, deserunt. Neque, officia unde
-      quia, voluptas tempore voluptates quo ipsam eaque commodi odio officiis.
-      Iste eveniet nisi ratione sed deserunt nemo consectetur eius repellendus
-      earum cum voluptatem debitis quos assumenda nam accusamus, et fugiat est
-      molestias. Dolore sed dolorem itaque doloremque, quidem harum expedita
-      neque necessitatibus est eveniet autem vero eius nam quisquam voluptatem
-      magni iusto laudantium vitae ipsum doloribus rerum, a quibusdam
-      temporibus. Laudantium sed voluptates velit deserunt, harum, quae
-      necessitatibus corporis fugiat quo consequuntur soluta ex omnis iusto
-      facilis inventore! Aut esse blanditiis non repudiandae velit tenetur,
-      quidem omnis consequatur recusandae amet assumenda, commodi sapiente,
-      porro natus sunt libero illum error nisi beatae ad voluptas possimus quis
-      hic iusto? Molestias quibusdam veritatis minus voluptatum nobis quam aut
-      sequi laboriosam facere suscipit? Tempore molestiae accusamus ea possimus
-      unde aliquid explicabo libero iusto sed minus suscipit accusantium
-      eligendi, amet nesciunt tempora quidem, voluptatem assumenda nostrum nam.
-      Corrupti atque fuga repellat, error ut quae neque excepturi soluta
-      laboriosam sapiente officia, expedita aliquam autem cumque quam tenetur
-      eligendi necessitatibus incidunt id suscipit quisquam dicta ad? Corrupti,
-      officia neque, corporis reprehenderit atque ullam veritatis eveniet
-      mollitia nostrum harum earum quos optio nam natus nisi molestias maiores
-      aliquid est unde? Ipsa magni accusamus eligendi.
+      <img src="./src/assets/images/stock3.jpg" className="gallery-img" />
     </div>
   );
 }
