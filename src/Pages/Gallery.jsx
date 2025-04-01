00d633bee918decf74839e033a50410026b8d546
nancymcw <nancymcwhite@gmail.com>
2025-04-01 13:09:52 -0400

    Added more stock images to gallery-div and max height to gallery-img in index.css

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,7 +1,10 @@
 export function Gallery() {
   return (
     <div className="gallery-div">
+      <img src="./src/assets/images/stock2.jpg" className="gallery-img" />
       <img src="./src/assets/images/stock3.jpg" className="gallery-img" />
+      <img src="./src/assets/images/stock4.jpg" className="gallery-img" />
+      <img src="./src/assets/images/stock5.jpg" className="gallery-img" />
     </div>
   );
 }
