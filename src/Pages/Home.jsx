ffb54464e4c5f72a1246478ae9ca28fde52c8c4a
nancymcw <nancymcwhite@gmail.com>
2025-04-01 22:58:28 -0400

    Changed end line of Home-wrapper div

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,15 +5,17 @@ import ServicesList from "../Components/ServicesList";
 
 export function Home() {
   return (
-    <div className="home-wrapper">
-      <div className="home-div">
-        <h1>BRINGING YOU QUALITY HOME REFINISHING SINCE 2016...</h1>
-        We strive for excellence every time we refinish or reface your cabinets.
-        Quality finishing and cabinets are a smart investment in your property.
-        From choosing the right colors and brand of paint for your job to the
-        actual painting and clean up, we provide value through our exceptional
-        service.
-        <ServicesList />
+    <>
+      <div className="home-wrapper">
+        <div className="home-div">
+          <h1>BRINGING YOU QUALITY HOME REFINISHING SINCE 2016...</h1>
+          We strive for excellence every time we refinish or reface your
+          cabinets. Quality finishing and cabinets are a smart investment in
+          your property. From choosing the right colors and brand of paint for
+          your job to the actual painting and clean up, we provide value through
+          our exceptional service.
+          <ServicesList />
+        </div>
       </div>
       <ContactMe />
       <br />
@@ -22,6 +24,6 @@ export function Home() {
       </div>
       <br />
       <Footer />
-    </div>
+    </>
   );
 }
