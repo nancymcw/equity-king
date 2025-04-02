49a69d80ac20974e2b1e420399081ba7aa03a7c7
nancymcw <nancymcwhite@gmail.com>
2025-04-01 21:05:18 -0400

    Added links to source images on each stock photo

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -4,10 +4,18 @@ export function Gallery() {
   return (
     <>
       <div className="gallery-div">
-        <img src="./src/assets/images/stock2.jpg" className="gallery-img" />
-        <img src="./src/assets/images/stock3.jpg" className="gallery-img" />
-        <img src="./src/assets/images/stock4.jpg" className="gallery-img" />
-        <img src="./src/assets/images/stock5.jpg" className="gallery-img" />
+        <a href="./src/assets/images/stock2.jpg">
+          <img src="./src/assets/images/stock2.jpg" className="gallery-img" />
+        </a>
+        <a href="./src/assets/images/stock3.jpg">
+          <img src="./src/assets/images/stock3.jpg" className="gallery-img" />
+        </a>
+        <a href="./src/assets/images/stock4.jpg">
+          <img src="./src/assets/images/stock4.jpg" className="gallery-img" />
+        </a>
+        <a href="./src/assets/images/stock5.jpg">
+          <img src="./src/assets/images/stock5.jpg" className="gallery-img" />
+        </a>
       </div>
       <Footer />
     </>
