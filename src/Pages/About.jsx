0de420fff2482411d5047af22d4620fcc4433c68
nancymcw <nancymcwhite@gmail.com>
2025-04-03 11:44:05 -0400

    Adding more styling to about page including renaming

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,15 +5,23 @@ import { Row } from "react-bootstrap";
 export function About() {
   return (
     <Container className="about-wrapper">
-      <Row className="about-bio-row">
-        <Col md={6}>
+      <Row className="about-row">
+        <Col md={6} className="photo-column" id="column">
           <Image
             src="./src/assets/images/john.jpg"
             fluid
             className="john-image"
           />
         </Col>
-        <Col md={6}>Column 2</Col>
+        <Col md={6} className="bio-column" id="column">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum modi
+          inventore unde eveniet quia, ipsam, sapiente accusantium numquam
+          ipsum, excepturi corporis debitis sit ratione incidunt dolore magnam
+          non fugit consectetur suscipit sunt. Aliquid perspiciatis voluptatum
+          itaque dolore dolores qui quod a provident aspernatur animi, autem
+          earum velit aliquam eligendi quo natus id ullam, unde fugit rerum,
+          optio atque ipsam laborum.
+        </Col>
       </Row>
       {/* <div className="about-bio">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
