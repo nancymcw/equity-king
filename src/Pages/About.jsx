926298de22ee641555182c097598dc47bc7fbc93
nancymcw <nancymcwhite@gmail.com>
2025-04-03 11:34:17 -0400

    React bootstrap columns on about page, image added to column 1

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,22 @@
+import { Container, Image } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+
 export function About() {
   return (
-    <div className="about-wrapper">
-      <div className="about-bio">
+    <Container className="about-wrapper">
+      <Row className="about-bio-row">
+        <Col md={6}>
+          <Image src="./src/assets/images/john.jpg" fluid />
+        </Col>
+        <Col md={6}>Column 2</Col>
+      </Row>
+      {/* <div className="about-bio">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
         reprehenderit, eos nihil, officia delectus similique, ratione iure
         quibusdam a aliquam dicta deserunt modi in harum consequuntur numquam
         debitis at sapiente tempora? Et, ad!
-      </div>
-    </div>
+      </div> */}
+    </Container>
   );
 }
