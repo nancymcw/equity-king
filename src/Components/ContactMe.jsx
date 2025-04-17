57af53c0efc6c9363ed440ea2f9c5ada0c9f0f5d
nancymcw <nancymcwhite@gmail.com>
2025-04-17 10:24:58 -0400

    Added styling for form in ContactMe, added phone number input

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -14,9 +14,30 @@ export function ContactMe() {
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" name="name" required />
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          pattern=".+@example\.com"
+          required
+        />
+        <label htmlFor="tel">Phone Number:</label>
+        <input
+          inputMode="numeric"
+          placeholder="(###)-###-####"
+          type="tel"
+          id="tel"
+          name="tel"
+          required
+        />
         <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" rows="4" required></textarea>
+        <textarea
+          id="message"
+          type="text"
+          name="message"
+          rows="4"
+          required
+        ></textarea>
         <button type="submit">Send</button>
       </form>
     </>
